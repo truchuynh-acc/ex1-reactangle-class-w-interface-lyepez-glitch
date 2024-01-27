9e9354c11dd7fa9f0ac4b87ffae8ba493713f71c
Lucas Yepez <80136179+lyepez-glitch@users.noreply.github.com>
2024-01-27 01:25:20 -0600

    Update Rectangle.cpp

--- Rectangle.cpp
+++ Rectangle.cpp
@@ -1,1 +1,34 @@
-/**Rectangle.cpp Code*/
+/** @file Rectangle.cpp */
+#include "Rectangle.h"
+
+
+Rectangle::Rectangle()
+{
+	length = 1.0;
+	width = 1.0;
+}
+
+bool Rectangle::set(double lngth,double wdth){
+	if(lngth>0.0 && wdth > 0.0){
+		length = lngth;
+		width = wdth;
+		return true;
+	}else{
+		return false;
+	}
+}
+double Rectangle::getLength() const
+{
+	return length;
+}
+double  Rectangle::getWidth() const
+{
+	return width;
+}
+double  Rectangle::getArea() const
+{
+	double area = length * width;
+	return area;
+}
+
+
