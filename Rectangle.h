771af30ee3fe0e94261b19ffe1a83e1a4ccb8fd6
Lucas Yepez <80136179+lyepez-glitch@users.noreply.github.com>
2024-01-27 01:24:45 -0600

    Update Rectangle.h

--- Rectangle.h
+++ Rectangle.h
@@ -1,1 +1,19 @@
-/**Rectangle.h Code*/
+/** @file Rectangle.h */
+#include "Rectangle_Interface.h"
+
+#ifndef RECTANGLE
+#define RECTANGLE
+
+
+class Rectangle : public RectangleInterface{
+	public:
+		Rectangle();
+		double length;
+		double width;
+	     virtual bool set(double lngth,double wdth);
+		double getLength() const;
+		double getWidth() const;
+		double getArea() const;
+};
+#include "Rectangle.cpp"
+#endif
